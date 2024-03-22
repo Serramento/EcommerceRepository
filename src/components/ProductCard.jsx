027fd8c1b767af3380682e85ab96d1d4bc691fb2
Serramento <serrasarihasan1@gmail.com>
2024-03-22 20:08:28 +0300

    updated2 #5

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,24 +9,17 @@ const ProductCard = (props) => {
         <h6 className="font-bold text-sm text-[#737373] mt-3">
           English Department
         </h6>
-        <h5 className="font-bold mt-5 mb-10">
+        <h5 className="font-bold mt-5 mb-3">
           <span className="text-[#BDBDBD]">$16.48</span>{" "}
           <span className="text-[#23856D]">$6.48</span>
         </h5>
       </div>
-      <div className="flex flex-row justify-center">
-        <svg height="100" width="100">
-          <circle r="9" cx="50" cy="50" fill="#23A6F0" />
-        </svg>
-        <svg height="100" width="100">
-          <circle r="9" cx="50" cy="50" fill="#23856D" />
-        </svg>
-        <svg height="100" width="100">
-          <circle r="9" cx="50" cy="50" fill="#E77C40" />
-        </svg>
-        <svg height="100" width="100">
-          <circle r="9" cx="50" cy="50" fill="#252B42" />
-        </svg>
+
+      <div className="flex justify-center mb-16">
+        <div className="h-4 w-4 rounded-lg bg-[#23A6F0] mr-1.5" />
+        <div className="h-4 w-4 rounded-lg bg-[#23856D] mr-1.5" />
+        <div className="h-4 w-4 rounded-lg bg-[#E77C40] mr-1.5" />
+        <div className="h-4 w-4 rounded-lg bg-[#252B42]" />
       </div>
     </a>
   );
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -30,9 +30,26 @@ function ShopPage() {
             <ProductCard product={product} />
           ))}
         </div>
+        <div className="rounded-lg border-2 border-[#BDBDBD] mb-20 mt-3 text-sm font-bold">
+          <button className="bg-[#F3F3F3] px-6 py-6 text-[#BDBDBD] rounded-l-lg rounded-r-none">
+            First
+          </button>
+          <button className="px-4 py-6 text-[#23A6F0] border-2 border-[#E9E9E9] rounded-none">
+            1
+          </button>
+          <button className="px-4 py-6 text-[#FFFFFF] rounded-none bg-[#23A6F0]">
+            2
+          </button>
+          <button className="px-4 py-6 text-[#23A6F0] border-2 border-[#E9E9E9] rounded-none">
+            3
+          </button>
+          <button className="px-6 py-6 text-[#23A6F0] rounded-r-lg rounded-l-none">
+            Next
+          </button>
+        </div>
       </div>
 
-      <div className="text-[#737373] flex flex-col lg:flex-row justify-evenly py-32 lg:px-24 lg:py-16 lg:scale-[0.6]">
+      <div className="text-[#737373] flex flex-col lg:flex-row justify-evenly py-24 lg:px-24 lg:py-16 lg:scale-[0.6] bg-[#FAFAFA]">
         <i class="fa-brands fa-hooli fa-7x "></i>
         <i class="fa-brands fa-lyft fa-7x max-[1023px]:pt-5 lg:pl-24"></i>
         <i class="fa-brands fa-pied-piper-hat fa-7x max-[1023px]:pt-5 lg:pl-24"></i>
