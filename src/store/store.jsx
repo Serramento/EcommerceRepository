b5c42989c1457772e449596863781190d3088440
Serramento <serrasarihasan1@gmail.com>
2024-06-11 20:59:02 +0300

    updated4 #18

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,8 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import { clientreducer } from "../reducers/clientReducer";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import * as clientReducer from "../reducers/clientReducer";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-export const store = createStore(
-  clientreducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+export const store = createStore(() => {
+  clientReducer, composeWithDevTools(applyMiddleware(thunk));
+});
